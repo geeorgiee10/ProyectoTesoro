a73bc724d4af21e9fc208c31f18a62d45d29c595
Jorge Arcoya López <georgiearclopez@gmail.com>
2025-11-12 11:53:28 +0100

    Añadir subir modelos dinamicamente

---
+++ Assets/Editor/CreateAssetsBundle.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using System.IO;
+using UnityEditor;
+using System;
+
+public class CreateAssetsBundles
+{
+
+    [MenuItem("Assets/Build AssetBundles")]
+    static void BuildAllAssetBundles()
+    {
+        Debug.Log("Building bundled asset");
+        try
+        {
+            string assetBundleDirectory = "Assets/StreamingAssets";
+            if (!Directory.Exists(Application.streamingAssetsPath))
+            {
+                Directory.CreateDirectory(assetBundleDirectory);
+            }
+
+            BuildPipeline.BuildAssetBundles(assetBundleDirectory, BuildAssetBundleOptions.None,
+              EditorUserBuildSettings.activeBuildTarget);
+
+            AssetDatabase.Refresh();
+
+        }
+        catch (Exception e)
+        {
+            Debug.Log(" failed to create bundled assets" + e);
+            throw;
+        }
+
+    }
+}
